967693b11d5ff09444d06d06328daf973720c490
Kiyarash Kia <kiarashkia1999@gmail.com>
2023-11-17 18:35:21 -0500

    Line module updated

---
+++ W8/lab/Line.cpp
@@ -0,0 +1,38 @@
+#define _CRT_NO_WARNINGS
+
+#include <iostream>
+#include "Line.h"
+
+using namespace std;
+namespace sdds {
+
+
+	Line::Line() {
+		m_length = 0;
+	}
+
+	Line::Line(const char* string, int length) 
+		: LblShape(string), m_length(length) {}
+
+	void Line::getSpecs(istream& is) {
+		LblShape::getSpecs(is);
+		is >> m_length;
+		is.ignore(1000, '\n');
+
+	}
+
+	void Line::draw(ostream& os) const {
+		if (m_length > 0 && label() != NULL)
+		{
+			os << label() << endl;
+			for (int i = 0; i < m_length; i++)
+			{
+				os << "=";
+			}
+			os << endl;
+		}
+	}
+
+
+
+}
--- W8/lab/Line.h
+++ W8/lab/Line.h
@@ -1,14 +1,22 @@
 #ifndef SDDS_LINE_H
 #define SDDS_LINE_H
 
-#include <iostream>
+#include "LblShape.h"
 
 namespace sdds {
 
+	class Line : public LblShape {
+		
+		int m_length;
+		
+	public:
+		Line();
+		Line(const char* string, int length);
 
+		void getSpecs(std::istream& is) override;
+		void draw(std::ostream& os) const override;
 
-
-
+	};
 
 
 }
